8e3082309a83b286f4da2e58331fbb2961c48d79
Champ-Kitti <105689839+kittiumnatsomboon@users.noreply.github.com>
2025-03-06 16:48:01 +0700

    DropDown

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2256,5 +2256,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <script src="js/plugins.js"></script>
     <script src="js/script.js"></script>
+    <script src="{{asset('js/dropdown.js')}}"></script>
   </body>
 </html>

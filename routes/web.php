1d397a89543b1934443fd0b106ff3c7856f7b290
Champ-Kitti <105689839+kittiumnatsomboon@users.noreply.github.com>
2025-03-06 20:25:45 +0700

    Restfull api

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\api\Registercontroller;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/Register', [Registercontroller::class, 'index']);

4319ccb42bc4766e9097e5404f68f5f4b15ccec0
Champ-Kitti <105689839+kittiumnatsomboon@users.noreply.github.com>
2025-03-06 15:24:58 +0700

    แก้ไข Font

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,8 +13,8 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/vendor.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="{{URL::TO('css/vendor.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::TO('css/style.css')}}">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -284,10 +284,10 @@
     </header>
     @yield('content')
 
-    <script src="js/jquery-1.11.0.min.js"></script>
+    <script src="{{asset('js/jquery-1.11.0.min.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <script src="js/plugins.js"></script>
-    <script src="js/script.js"></script>
+    <script src="{{asset('js/plugins.js')}}"></script>
+    <script src="{{asset('js/script.js')}}"></script>
 </body>
 </html>

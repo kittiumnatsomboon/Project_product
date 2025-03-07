beeaebd1e1ffcb3cee78680ca25710637c8d360a
Champ-Kitti <105689839+kittiumnatsomboon@users.noreply.github.com>
2025-03-07 16:28:56 +0700

    ฟอร์มสมัครสมาชิก
    ฟอร์มสมัครสมาชิก

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -59,16 +59,137 @@
                             class="form-control form-control-lg" name="housenumber" id="housenumber" placeholder="กรุณาระบุบ้านเลขที่">
                         </div>
                         <div class="mb-3">
+                            <label for="name" class="form-label">หมู่ที่</label>
+                            <input type="text"
+                            class="form-control form-control-lg" name="VillageNumber" id="VillageNumber" placeholder="กรุณาระบุหมู่ที่ของบ้าน">
+                        </div>
+                        <div class="mb-3">
+                            <label for="name" class="form-label">ซอย</label>
+                            <input type="text"
+                            class="form-control form-control-lg" name="Alley" id="Alley" placeholder="กรุณาระบุหมู่ที่ของบ้าน">
+                        </div>
+                        <div class="mb-3">
+                            <label for="name" class="form-label">ถนน</label>
+                            <input type="text"
+                            class="form-control form-control-lg" name="Road" id="Road" placeholder="กรุณาระบุถนนสายอะไรหรือเส้นอะไร">
+                        </div>
+                        <div class="mb-3">
+                            <label for="name" class="form-label">ตำบล/แขวง</label>
+                            <input type="text"
+                            class="form-control form-control-lg" name="Sub-district" id="Sub-district" placeholder="กรุณาระบุตำบลหรือแขวงที่ท่านอาศัยอยู่">
+                        </div>
+                        <div class="mb-3">
+                            <label for="name" class="form-label">อำเภอ/เขต</label>
+                            <input type="text"
+                            class="form-control form-control-lg" name="District" id="District" placeholder="กรุณาระบุอำเภอ/เขตที่อาศัยอยู่">
+                        </div>
+                        <div class="mb-3">
+                            <label for="name" class="form-label">จังหวัด</label>
+                            <select id="province" class="form-select" aria-label="Default select example">
+                                <option value="">-- กรุณาเลือกจังหวัด --</option>
+                                <option value="กรุงเทพมหานคร">กรุงเทพมหานคร</option>
+                                <option value="กระบี่">กระบี่</option>
+                                <option value="กาญจนบุรี">กาญจนบุรี</option>
+                                <option value="กาฬสินธุ์">กาฬสินธุ์</option>
+                                <option value="กำแพงเพชร">กำแพงเพชร</option>
+                                <option value="ขอนแก่น">ขอนแก่น</option>
+                                <option value="จันทบุรี">จันทบุรี</option>
+                                <option value="ฉะเชิงเทรา">ฉะเชิงเทรา</option>
+                                <option value="ชลบุรี">ชลบุรี</option>
+                                <option value="ชัยนาท">ชัยนาท</option>
+                                <option value="ชัยภูมิ">ชัยภูมิ</option>
+                                <option value="ชุมพร">ชุมพร</option>
+                                <option value="เชียงราย">เชียงราย</option>
+                                <option value="เชียงใหม่">เชียงใหม่</option>
+                                <option value="ตรัง">ตรัง</option>
+                                <option value="ตราด">ตราด</option>
+                                <option value="ตาก">ตาก</option>
+                                <option value="นครนายก">นครนายก</option>
+                                <option value="นครปฐม">นครปฐม</option>
+                                <option value="นครพนม">นครพนม</option>
+                                <option value="นครราชสีมา">นครราชสีมา</option>
+                                <option value="นครศรีธรรมราช">นครศรีธรรมราช</option>
+                                <option value="นครสวรรค์">นครสวรรค์</option>
+                                <option value="นนทบุรี">นนทบุรี</option>
+                                <option value="นราธิวาส">นราธิวาส</option>
+                                <option value="น่าน">น่าน</option>
+                                <option value="บึงกาฬ">บึงกาฬ</option>
+                                <option value="บุรีรัมย์">บุรีรัมย์</option>
+                                <option value="ปทุมธานี">ปทุมธานี</option>
+                                <option value="ประจวบคีรีขันธ์">ประจวบคีรีขันธ์</option>
+                                <option value="ปราจีนบุรี">ปราจีนบุรี</option>
+                                <option value="ปัตตานี">ปัตตานี</option>
+                                <option value="พระนครศรีอยุธยา">พระนครศรีอยุธยา</option>
+                                <option value="พะเยา">พะเยา</option>
+                                <option value="พังงา">พังงา</option>
+                                <option value="พัทลุง">พัทลุง</option>
+                                <option value="พิจิตร">พิจิตร</option>
+                                <option value="พิษณุโลก">พิษณุโลก</option>
+                                <option value="เพชรบุรี">เพชรบุรี</option>
+                                <option value="เพชรบูรณ์">เพชรบูรณ์</option>
+                                <option value="แพร่">แพร่</option>
+                                <option value="ภูเก็ต">ภูเก็ต</option>
+                                <option value="มหาสารคาม">มหาสารคาม</option>
+                                <option value="มุกดาหาร">มุกดาหาร</option>
+                                <option value="แม่ฮ่องสอน">แม่ฮ่องสอน</option>
+                                <option value="ยโสธร">ยโสธร</option>
+                                <option value="ยะลา">ยะลา</option>
+                                <option value="ร้อยเอ็ด">ร้อยเอ็ด</option>
+                                <option value="ระนอง">ระนอง</option>
+                                <option value="ระยอง">ระยอง</option>
+                                <option value="ราชบุรี">ราชบุรี</option>
+                                <option value="ลพบุรี">ลพบุรี</option>
+                                <option value="ลำปาง">ลำปาง</option>
+                                <option value="ลำพูน">ลำพูน</option>
+                                <option value="เลย">เลย</option>
+                                <option value="ศรีสะเกษ">ศรีสะเกษ</option>
+                                <option value="สกลนคร">สกลนคร</option>
+                                <option value="สงขลา">สงขลา</option>
+                                <option value="สตูล">สตูล</option>
+                                <option value="สมุทรปราการ">สมุทรปราการ</option>
+                                <option value="สมุทรสงคราม">สมุทรสงคราม</option>
+                                <option value="สมุทรสาคร">สมุทรสาคร</option>
+                                <option value="สระแก้ว">สระแก้ว</option>
+                                <option value="สระบุรี">สระบุรี</option>
+                                <option value="สิงห์บุรี">สิงห์บุรี</option>
+                                <option value="สุโขทัย">สุโขทัย</option>
+                                <option value="สุพรรณบุรี">สุพรรณบุรี</option>
+                                <option value="สุราษฎร์ธานี">สุราษฎร์ธานี</option>
+                                <option value="สุรินทร์">สุรินทร์</option>
+                                <option value="หนองคาย">หนองคาย</option>
+                                <option value="หนองบัวลำภู">หนองบัวลำภู</option>
+                                <option value="อำนาจเจริญ">อำนาจเจริญ</option>
+                                <option value="อุดรธานี">อุดรธานี</option>
+                                <option value="อุตรดิตถ์">อุตรดิตถ์</option>
+                                <option value="อุทัยธานี">อุทัยธานี</option>
+                                <option value="อุบลราชธานี">อุบลราชธานี</option>
+                                <option value="อ่างทอง">อ่างทอง</option>
+                            </select>   
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">รหัสไปษรณี</label>
+                            <input type="text" class="form-control form-control-lg" name="PostalCode" id="PostalCode" placeholder="กรุณาระบุรหัสไปรษณี">
+
+                        </div>
+                        <div class="mb-3">
                             <label for="" class="form-label">Email</label>
-                            <input type="email" class="form-control form-control-lg" name="email" id="email" placeholder="abc@mail.com">
+                            <input type="email" class="form-control form-control-lg" name="email" id="email" placeholder="example@mail.com">
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">Password</label>
+                            <input type="password" class="form-control form-control-lg" name="password" id="password" placeholder="กรุณาระบุรหัสผ่าน">
+                        </div>
+                        <div class="mb-3">
+                            <label for="" class="form-label">confirm password</label>
+                            <input type="password" class="form-control form-control-lg" name="confirm password" id="confirm password" placeholder="กรุณาระบุรหัสผ่านอีกครั้ง">
                         </div>
                         <div class="form-check form-check-inline mb-3">
                             <label class="form-check-label" for="subscribe">
                             <input class="form-check-input" type="checkbox" id="subscribe" value="subscribe">
-                            Subscribe to the newsletter</label>
+                            ยืนยันให้ทำการจัดเก็บข้อมูลข้างต้น</label>
                         </div>
                         <div class="d-grid gap-2">
-                            <button type="submit" class="btn btn-dark btn-lg">Submit</button>
+                            <button type="submit" class="btn btn-dark btn-lg">สมัครสมาชิก</button>
                         </div>
                         </form>
                         
